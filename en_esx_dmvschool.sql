031f250ee869c2b42cff533eb38284c6aefc5291
PanScoobiik <68381744+PanScoobiik@users.noreply.github.com>
2020-08-18 13:01:22 +0200

    Initial commit

---
+++ en_esx_dmvschool.sql
@@ -0,0 +1,8 @@
+USE `essentialmode`;
+
+INSERT INTO `licenses` (`type`, `label`) VALUES
+	('dmv', 'Řidičské oprávnění'),
+	('drive', 'Řidičský průkaz'),
+	('drive_bike', 'Řidičský průkaz pro motorky'),
+	('drive_truck', 'Řidičský průkaz na kamion')
+;
---
+++ html/questions.js
@@ -0,0 +1,94 @@
+var tableauQuestion = [
+
+	{
+		question: "Jak sa zachovas pokud odbocujes doprava na svetelne krizovatce a vidis chodce jit pres prechod:",
+		propositionA: "Vyhnes se chodcovi a pokracujes",
+		propositionB: "Skontrolujes jestli v krizovatce nejsou jine vozidla",
+		propositionC: "Pockas az chodec prejde",
+		propositionD: "Zastrelis chodce a pokracujes v jizde",
+		reponse: "C"
+	},
+
+	{
+		question: "Jaka je maximalni povolena rychlost v obytne casti mesta?",
+		propositionA: "40 km/h",
+		propositionB: "50 km/h",
+		propositionC: "30 km/h",
+		propositionD: "80 km/h",
+		reponse: "B"
+	},
+
+	{
+		question: "Pokud jedes 80 mp/h a vjizdis do obytne casti mesta musis:",
+		propositionA: "Zrychlit.",
+		propositionB: "Udrzujes si rychlost pokud nepredjizdis jine vozidlo.",
+		propositionC: "Spomalit",
+		propositionD: "Udrzujes si rychlost",
+		reponse: "C"
+	},
+
+	{
+		question: "Pred zmenou jizdnich pruhu musis:",
+		propositionA: "Útočit na ostatní",
+		propositionB: "Zkontrolovat mrtvy uhel",
+		propositionC: "Pouzit smerovky pri zmene jizdnich pruhu",
+		propositionD: "Vsechno nad touto odpovedi",
+		reponse: "D"
+	},
+
+	{
+		question: "Jaky je povoleny obsah alkoholu v krvi pri rizeni?",
+		propositionA: "0.00%",
+		propositionB: "0.18%",
+		propositionC: "2.08%",
+		propositionD: "0.06%",
+		reponse: "A"
+	},
+
+	{
+		question: "Kdyz te jine vozidlo predjizdi, ty NESMIS:",
+		propositionA: "Spomalit",
+		propositionB: "Skontrolovat zpetne zrcatka",
+		propositionC: "Sledovat ostatni ridice",
+		propositionD: "Zrychlovat",
+		reponse: "D"
+	},
+
+	{
+		question: "Kdy muzes prejizdet svetelnou krizovatkou?",
+		propositionA: "Kdyz sviti zelena",
+		propositionB: "Kdyz na mě všichni ostatní řvou",
+		propositionC: "Kdyz si v okoli skoly",
+		propositionD: "Kdyz sviti zelena nebo pokud svieti cervena ale odbocujes do prava a neohrozis provoz.",
+		reponse: "D"
+	},
+
+	{
+		question: "Chodec ma znameni stat na prechode. Co udelas?",
+		propositionA: "Nechas ho prejit",
+		propositionB: "Začneš na chodce křičet",
+		propositionC: "Reknes mu at prejde pres prechod",
+		propositionD: "Pokracujes v jizde protoze sviti zelena",
+		reponse: "D"
+	},
+
+	{
+		question: "Co je povolene kdyz predjizdis jine vozidlo?",
+		propositionA: "Dotahnes se na neho aby si ho rychleji predjel",
+		propositionB: "Predjedes ho bez opusteni vozovky",
+		propositionC: "Predjedes ho pres vedlejsi pruh",
+		propositionD: "Prekrocis rychlostni limit pri predjizdeni",
+		reponse: "C"
+	},
+
+	{
+		question: "Na dialnici je povolena maximalni rychlost 75 mp/h. Ale vetsina lidi jede 80 mp/h, ty by si nemel jet rychleji jak:",
+		propositionA: "75 km/h",
+		propositionB: "70 km/h",
+		propositionC: "80 km/h",
+		propositionD: "150 km/h",
+		reponse: "A"
+	},
+
+]
+
